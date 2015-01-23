95da1cf9788b3eea651d8a39f752f6b6b00783da
Michael C. Libby <mcl@vizsla>
2015-01-23 09:06:12 -0600

    rebind solvents in UI

--- src/js/model/hplc_compound.js
+++ src/js/model/hplc_compound.js
@@ -2,23 +2,31 @@
 
 var HPLC = require("./hplc_globals.js");
 
+/*
+log(k')W1 = km * temperature + kb
+S = sm * temperature + sb
+*/
 var Compound = function (compoundName, solventName, concentration) {
   this.name = compoundName;
   this.concentration = concentration;
 
   var compound =  compoundProperties[compoundName];
-  this.molarVolume = compound.molarVolume;
+  this.mvol = compound.mvol;
   
   var properties = compound[solventName];
-  this.logKwTslope = properties.logKwTslope;
-  this.logKwTintercept = properties.logKwTintercept;
-  this.sTslope = properties.sTslope;
-  this.sTintercept = properties.sTintercept;
+  this.km = properties.km;
+  this.kb = properties.kb;
+  this.sm = properties.sm;
+  this.sb = properties.sb;
+
+  Object.defineProperty(this, 'molarVolume', {
+    get: function () { return this.mvol; }
+  });
 };
 
 Compound.prototype.kprime = function (fraction, temperature) {
-  var logkprimew1 = this.logKwTslope * temperature + this.logKwTintercept;
-  var s1 = -1 * ((this.sTslope * temperature) + this.sTintercept);
+  var logkprimew1 = this.km * temperature + this.kb;
+  var s1 = -1 * ((this.sm * temperature) + this.sb);
   var kprime = Math.pow(10, logkprimew1 - (s1 * fraction));
   return kprime;
 };
@@ -28,95 +36,95 @@ exports.Compound = Compound;
 /* New Compound Entry Template */
 // 'compound': {
 //   'Acetonitrile': {
-//      logKwTslope: ,
-//      logKwTintercept: ,
-//      sTslope: ,
-//      sTintercept:
+//      km: ,
+//      kb: ,
+//      sm: ,
+//      sb:
 //   },
 //   'Methanol': {
-//      logKwTslope: ,
-//      logKwTintercept: ,
-//      sTslope: ,
-//      sTintercept:
+//      km: ,
+//      kb: ,
+//      sm: ,
+//      sb:
 //   }
 // },
 
 /* TODO: load from database or file */
 
 var compoundProperties = {
-  phenol: {
-    molarVolume: 103.4,
+  'phenol': {
+    mvol: 103.4,
     'Acetonitrile': {
-      logKwTslope: -0.007051397,
-      logKwTintercept: 1.222652803,
-      sTslope: 0.004948239,
-      sTintercept: -2.157819856
+      km: -0.007051397,
+      kb: 1.222652803,
+      sm: 0.004948239,
+      sb: -2.157819856
     },
     'Methanol': {
-      logKwTslope: -0.010465,
-      logKwTintercept: 1.714002,
-      sTslope: 0.009040,
-      sTintercept: -2.668850
+      km: -0.010465,
+      kb: 1.714002,
+      sm: 0.009040,
+      sb: -2.668850
     },
   },
   '3-phenyl propanol': {
-    molarVolume: 170.0,
+    mvol: 170.0,
     'Acetonitrile': {
-      logKwTslope: -0.005175387,
-      logKwTintercept: 1.617423196,
-      sTslope: 0.004245094,
-      sTintercept: -2.711627278
+      km: -0.005175387,
+      kb: 1.617423196,
+      sm: 0.004245094,
+      sb: -2.711627278
     },
     'Methanol': {
-      logKwTslope: -0.012422,
-      logKwTintercept: 2.682599,
-      sTslope: 0.010667,
-      sTintercept: -3.544294
+      km: -0.012422,
+      kb: 2.682599,
+      sm: 0.010667,
+      sb: -3.544294
     }
   },
   'acetophenone': {
-    molarVolume: 140.4,
+    mvol: 140.4,
     'Acetonitrile': {
-      logKwTslope: 0,
-      logKwTintercept: 0,
-      sTslope: 0,
-      sTintercept:0
+      km: -0.006113393,
+      kb: 1.615282733,
+      sm: 0.004190421,
+      sb: -2.419171414
     },
     'Methanol': {
-      logKwTslope: 0,
-      logKwTintercept: 0,
-      sTslope: 0,
-      sTintercept:0
+      km: -0.009257,
+      kb: 2.098172,
+      sm: 0.008684,
+      sb: -2.981032
     }
   },
   'p-chlorophenol': {
-    molarVolume: 124.3,
+    mvol: 124.3,
     'Acetonitrile': {
-      logKwTslope: 0,
-      logKwTintercept:0 ,
-      sTslope: 0,
-      sTintercept:0
+      km: -0.009910541,
+      kb: 2.006666967,
+      sm: 0.009176534,
+      sb: -3.110178571
     },
     'Methanol': {
-      logKwTslope: 0,
-      logKwTintercept:0 ,
-      sTslope: 0,
-      sTintercept:0
+      km: -0.015142,
+      kb: 2.752714,
+      sm: 0.013319,
+      sb: -3.575982
     }
   },
   'p-nitrotoluene': {
-    molarVolume: 144.9,
+    mvol: 144.9,
     'Acetonitrile': {
-      logKwTslope: 0,
-      logKwTintercept:0 ,
-      sTslope: 0,
-      sTintercept:0
+      km: -0.010705359,
+      kb: 2.565390235,
+      sm: 0.009864271,
+      sb: -3.433694873
     },
     'Methanol': {
-      logKwTslope: 0,
-      logKwTintercept: 0,
-      sTslope: 0,
-      sTintercept:0
+      km: -0.012887,
+      kb: 2.900254,
+      sm: 0.011005,
+      sb: -3.522154
     }
   },
 };
